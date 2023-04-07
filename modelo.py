1dbb3f4ae28b85b70176963378807169f53d5b65
Fawke-s <83735637+Marlon-Santiago@users.noreply.github.com>
2023-04-06 22:03:51 -0300

    Usando o polimorfismo e usando representção de str

--- modelo.py
+++ modelo.py
@@ -11,7 +11,7 @@ class Programa:
      @property
      def likes(self):
         return self._likes
-    
+     
      @nome.setter
      def nome(self, novo_nome):
         self._nome = novo_nome.titled()
@@ -19,10 +19,16 @@ class Programa:
      def dar_like(self):
         self._likes += 1
 
+     def __str__(self):
+         return f'Nome --> {self._nome} - Ano --> {self.ano} - Likes --> {self._likes}'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+         return f'Nome --> {self._nome} - Ano --> {self.ano} - Duração --> {self.duracao} Min - Likes --> {self._likes}'
 
         
 class Serie(Programa):
@@ -30,18 +36,19 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+         return f'Nome --> {self._nome} - Ano --> {self.ano} - Temporada --> {self.temporadas} Temporadas - Likes --> {self._likes}'
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
-
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
 
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
-    print(f'{programa.nome} - {detalhes} D  - {programa.likes}')
+   print(programa)
+
