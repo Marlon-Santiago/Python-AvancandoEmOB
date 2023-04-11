8246f2cfa0abac1d60a19f365ee1109ae79b6e9f
Fawke-s <83735637+Marlon-Santiago@users.noreply.github.com>
2023-04-10 23:00:16 -0300

    implementando Playlist
    criando a classe playlist, transformando playlist em um iterável, verificando se o item esta dentro da lista,

--- modelo.py
+++ modelo.py
@@ -40,15 +40,48 @@ class Serie(Programa):
          return f'Nome --> {self._nome} - Ano --> {self.ano} - Temporada --> {self.temporadas} Temporadas - Likes --> {self._likes}'
 
 
+class Playlist:
+    def __init__(self, nome, programa):
+      self.nome = nome.title()
+      self._programa = programa
+
+    @property
+    def listagem (self):
+        return self._programa
+        
+    @property
+    def tamanho (self):
+        return len(self._programa)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
+tdmp = Filme('Todo mundo em panico', 1999, 100)
 atlanta = Serie('atlanta', 2018, 2)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+vingadores.dar_like()
+tdmp.dar_like()
+tdmp.dar_like()
+tdmp.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+vingadores.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta, demolidor, tdmp]
+playlist_fds = Playlist('Fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+print(f'Tamanho do playlist {len(playlist_fds.listagem)}')
+
+print(demolidor in playlist_fds.listagem)
+
+for programa in playlist_fds.listagem:
    print(programa)
 
